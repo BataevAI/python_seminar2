3aeda68676bf40b23181d2f20e31ad3a20f926d1
Andrey Bataev <bataevai@gmail.com>
2023-02-24 10:38:22 +0400

    Exc.12

--- Exc.12/Exc.12.py
+++ Exc.12/Exc.12.py
@@ -8,4 +8,23 @@
 # 12
 # Вывод
 # 3 4 или 4 3
+import math
+from random import randint
 
+x = randint(1, 1000)
+# print(f'Первое число : {x}')
+
+y = randint(1, 1000)
+# print(f'Второе число : {y}')
+summa = x + y
+print(f'Сумма чисел: {summa}')
+proizved = x * y
+print(f'Произведение чисел: {proizved}')
+
+discriminant = math.sqrt(summa * summa - 4 * proizved)
+if discriminant < 0: print("таких натуральных чисел не существует")
+elif discriminant == 0: print(f'Оба числа равны {int(summa/2)}')
+else:
+    x1 = int((summa + discriminant) / 2)
+    y1 = int((summa - discriminant) / 2)
+    print(f'Загаданные числа соответственно равны: {x1} и {y1}')
